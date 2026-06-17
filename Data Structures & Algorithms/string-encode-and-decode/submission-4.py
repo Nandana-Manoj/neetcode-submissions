bcd79bfb6c343736ee8af9fcf616ee0887762dc5
Nandana-Manoj <79393932+Nandana-Manoj@users.noreply.github.com>
2026-06-17 21:42:24 +0530

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        # combined = "-".join(strs)
+        # print("Encoded:", combined)
+        # return combined
+        combined = ""
+        for i in strs:
+            combined += str(len(str(len(i)))) + str(len(i)) + i
+        return combined
+
+    def decode(self, s: str) -> List[str]:
+        if not len(s):
+            return []
+        
+        strs = []
+        num_digits = int(s[0])
+        word_len = int(s[1:1+num_digits])
+        i = 1 + num_digits
+        while True:
+            j = i+word_len
+            word = s[i:j]
+            strs.append(word)
+            if i+word_len > len(s) - 1:
+                break
+            num_digits = int(s[j])
+            i = i+word_len+1+num_digits
+            
+            word_len = int(s[j+1:j+1+num_digits])
+            
+        return strs
